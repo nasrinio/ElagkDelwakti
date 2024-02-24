19a65b8998d0a4dd099a0d3f6525fae6571346a3
nasrinio <86842564+nasrinio@users.noreply.github.com>
2024-02-24 20:59:22 +0200

    a5r mara

--- DB/connection.js
+++ DB/connection.js
@@ -1,8 +1,27 @@
+// import mongoose from "mongoose";
+
+// export const connectionDB = async () => {
+//   return await mongoose
+//     .connect(process.env.CONNECTION_DB_CLOUD)
+//     .then((res) => console.log("DB connection success"))
+//     .catch((err) => console.log("DB connection Fail", err));
+// };
 import mongoose from "mongoose";
 
-export const connectionDB = async () => {
-  return await mongoose
-    .connect(process.env.CONNECTION_DB_CLOUD)
-    .then((res) => console.log("DB connection success"))
-    .catch((err) => console.log("DB connection Fail", err));
+export const connectionDB = () => {
+  return new Promise((resolve, reject) => {
+    mongoose
+      .connect(process.env.CONNECTION_DB_CLOUD, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log("DB connection success");
+        resolve(); // Resolve the promise if the connection is successful
+      })
+      .catch((err) => {
+        console.log("DB connection Fail", err);
+        reject(err); // Reject the promise if there's an error in connecting
+      });
+  });
 };
