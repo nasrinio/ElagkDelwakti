5f344e3db44afac5a58c8bf9e3cc5c61343ba007
nasrinio <86842564+nasrinio@users.noreply.github.com>
2024-02-24 20:29:21 +0200

    momo

--- DB/connection.js
+++ DB/connection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectionDB = async () => {
   return await mongoose
     .connect(process.env.CONNECTION_DB_CLOUD, {
-      serverSelectionTimeoutMS: 30000, // Increase server selection timeout
+      //serverSelectionTimeoutMS: 30000, // Increase server selection timeout
     })
     .then((res) => console.log("DB connection success"))
     .catch((err) => console.log("DB connection Fail", err));
